dfb14abc4e48b133157b4bd18bd661b17f744dc4
cghil <cghilarducci@gmail.com>
2015-07-05 15:05:41 -0700

    added axes for scatterplot

---
+++ chapter4/README.md
@@ -0,0 +1,9 @@
+#Generators
+- D3 generators consist of functions that take data and return the neccessary SVG drawing code to create a graphical object based on that data
+
+- ex. d3.svg.line and d3.svg.area
+
+#Components
+- Chart components for example... d3.svg.axis, which creates a bunch of <line>, <path>, <g> and <text> elements that are needed for an axis based on the scale and settings you provide the function
+
+- d3.svg.brush
---
+++ chapter4/example.js
@@ -0,0 +1,45 @@
+var scatterData = [{friends: 5, salary: 22000},
+{friends: 3, salary: 18000}, {friends: 10, salary: 88000},
+{friends: 0, salary: 180000}, {friends: 27, salary: 56000},
+{friends: 8, salary: 74000}];
+
+// d3.select("svg").selectAll("circle")
+// 	.data(scatterData).enter()
+// 	.append("circle").attr("r", 5).attr("cx", function(d, i){
+// 		return i * 10
+// 	}).attr("cy", function(d){
+// 		return d.friends;
+// 	});
+
+var xExtent = d3.extent(scatterData, function(d){
+	return d.salary;
+});
+// min and max array for salary
+
+var yExtent = d3.extent(scatterData, function(d){
+	return d.friends;
+});
+
+// min and max array for friends
+
+// the function d3.extent returns and array with the max and min values
+var xScale = d3.scale.linear().domain(xExtent).range([0, 500]);
+
+var yScale = d3.scale.linear().domain(yExtent).range([0, 500]);
+
+
+d3.select("svg").selectAll("circle")
+	.data(scatterData).enter().append("circle")
+	.attr("r", 5).attr("cx", function(d){
+		return xScale(d.salary);
+	}).attr("cy", function(d){
+		return yScale(d.friends)
+	})
+
+var yAxis = d3.svg.axis().scale(yScale).orient("right");
+d3.select("svg").append("g").attr("id", "yAxisG").call(yAxis);
+
+var xAxis = d3.svg.axis().scale(xScale).orient("bottom");
+d3.select("svg").append("g").attr("id", "xAxisG").call(xAxis);
+
+// .call()
